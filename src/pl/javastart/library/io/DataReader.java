7459b61aa18cd31191519fdd4a6cb6459358db6b
Dawid Kozlowski <dawid_kozlowski@o2.pl>
2025-10-16 14:02:52 +0200

    Add Magazine and Publication classes, update Library and DataReader to support magazines

--- src/pl/javastart/library/io/DataReader.java
+++ src/pl/javastart/library/io/DataReader.java
@@ -1,15 +1,12 @@
 package pl.javastart.library.io;
 
 import pl.javastart.library.model.Book;
+import pl.javastart.library.model.Magazine;
 
 import java.util.Scanner;
 
 public class DataReader {
     private Scanner sc = new Scanner(System.in);
-
-    public void close() {
-        sc.close();
-    }
 
     public int getInt() {
         int number = sc.nextInt();
@@ -18,21 +15,36 @@ public class DataReader {
     }
 
     public Book readAndCreateBook() {
-        System.out.println("Tytuł: ");
+        System.out.print("Tytuł: ");
         String title = sc.nextLine();
-        System.out.println("Autor: ");
+        System.out.print("Autor: ");
         String author = sc.nextLine();
-        System.out.println("Wydawnictwo: ");
+        System.out.print("Wydawnictwo: ");
         String publisher = sc.nextLine();
-        System.out.println("ISBN: ");
+        System.out.print("ISBN: ");
         String isbn = sc.nextLine();
-        System.out.println("Rok wydania: ");
-        int releaseDate = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Ilość stron: ");
-        int pages = sc.nextInt();
-        sc.nextLine();
+        System.out.print("Rok wydania: ");
+        int releaseDate = getInt();
+        System.out.print("Ilość stron: ");
+        int pages = getInt();
 
         return new Book(title, author, releaseDate, pages, publisher, isbn);
+    }
+
+    public Magazine readAndCreateMagazine() {
+        System.out.println("Tytuł: ");
+        String title = sc.nextLine();
+        System.out.println("Wydawnictwo: ");
+        String publisher = sc.nextLine();
+        System.out.println("Język: ");
+        String language = sc.nextLine();
+        System.out.println("Rok wydania: ");
+        int year = getInt();
+        System.out.println("Miesiąc: ");
+        int month = getInt();
+        System.out.println("Dzień: ");
+        int day = getInt();
+
+        return new Magazine(title, publisher, language, year, month, day);
     }
 }
