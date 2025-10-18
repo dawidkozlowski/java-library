e221089e93ce4319081fe30ddccf4f7dee22e355
Dawid Kozlowski <dawid_kozlowski@o2.pl>
2025-10-18 11:09:43 +0200

    Refactor model classes to use toString instead of printInfo and implement equals/hashCode methods

--- src/pl/javastart/library/model/Book.java
+++ src/pl/javastart/library/model/Book.java
@@ -1,12 +1,13 @@
 package pl.javastart.library.model;
 
+import java.util.Objects;
+
 public class Book extends Publication {
-    // Pola
+
     private String author;
     private int pages;
     private String isbn;
 
-    // Konstruktory
     public Book(String title, String author, int year, int pages, String publisher,
                 String isbn) {
         super(title, publisher, year);
@@ -15,7 +16,6 @@ public class Book extends Publication {
         this.isbn = isbn;
     }
 
-    // settery i gettery
     public String getIsbn() {
         return isbn;
     }
@@ -40,9 +40,24 @@ public class Book extends Publication {
         this.author = author;
     }
 
-    public void printInfo() {
-        String info = getTitle() + "; " + author + "; " + getYear() + "; "
-                + pages + "; " + getPublisher() + "; " + isbn;
-        System.out.println(info);
+    @Override
+    public String toString() {
+        return super.toString() + author + ", " + pages + ", " + isbn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Book book = (Book) o;
+        return pages == book.pages &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), author, pages, isbn);
     }
 }
